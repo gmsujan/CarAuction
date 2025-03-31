40e880d9d3d396a5abba6aac0539b9996d8dfe6b
gmsujan <sujanmagar6861@gmail.com>
2025-03-31 22:40:15 +1300

    Added the remaining files in all of the services and contracts

---
+++ SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("----> Consuming bid placed");
+
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if (context.Message.BidStatus.Contains("Accepted")
+                && context.Message.Amount > auction.CurrentHightBid)
+            {
+                auction.CurrentHightBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+        }
+    }
+}
